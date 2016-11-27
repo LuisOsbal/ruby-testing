4f756fface0cd94fcc3b84c4068bb67168858712
Luis Osnet <osnet@outlook.com>
2016-11-27 14:00:51 -0600

    Red, Green, Refactor method

--- tdd/bank_account.rb
+++ tdd/bank_account.rb
@@ -1,2 +1,13 @@
 class BankAccount
+
+	attr_accessor :amount 
+
+	def initialize(amount)
+		@amount = amount
+	end
+
+	def withdraw(amount)
+		raise ArgumentError.new("Insufficient funds") if @amount < amount
+		@amount -= amount
+	end
 end
--- tdd/bank_account_test.rb
+++ tdd/bank_account_test.rb
@@ -3,10 +3,29 @@ require "./bank_account"
 
 class TestBankAccount < MiniTest::Test
 	def setup
-		@account = BankAccount.new
+		@account = BankAccount.new(200)
 	end
 
 	def test_bank_account_is_not_nil
 		refute_nil @account
+	end
+
+	def test_account_has_a_withdraw_method
+		assert @account.respond_to?(:withdraw)
+	end
+
+	def test_withdraw_removes_money_from_account
+		@account.withdraw(100)
+		assert_equal 100, @account.amount
+	end
+
+	def test_cant_withdraw_more_money_that_it_has
+		assert_raises ArgumentError do
+			@account.withdraw(300)
+		end
+	end
+
+	def test_initial_value_for_bank_account
+		assert_equal @account.amount, 200
 	end
 end
